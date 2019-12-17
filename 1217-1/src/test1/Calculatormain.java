59eedb35e94097b5dcb2741bf9701c958ee9e66d
5 <5@5-PC>
2019-12-17 11:50:31 +0900

    12월 17일 오전

---
+++ 1217-1/src/test1/Calculatormain.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class Calculatormain {
+
+	public static void main(String[] args) {
+		Calculator ca = new Calculator();
+		ca.calpro();
+
+	}
+
+}
---
+++ 1217-1/src/test1/Car.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Car {
+	public static void main(String[] args) {
+				Gas gas = new Gas(); 
+				gas.input(); //주입 실행
+
+			}
+
+
+
+	}
