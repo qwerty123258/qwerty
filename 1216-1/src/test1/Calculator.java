9a6689610ba6c180c27d0f65e8512778012dbd36
5 <5@5-PC>
2019-12-17 09:15:56 +0900

    계산기~

--- 1216-1/src/test1/Calculator.java
+++ 1216-1/src/test1/Calculator.java
@@ -1,51 +1,95 @@
 package test1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Calculator {
 	
-	
-	public int sum(int num1, int num2) { //반환이 있는 메소드 int형으로 반환
-		int result; 
+	public void calpro() {
+		while(true) {
+		Scanner scan = new Scanner(System.in);
+		int num1=0;
+		int num2=0;
+		int result=0;
+		int command=0;
+		System.out.println("--------------------------");
+		System.out.println("1.덧셈 2.뺄셈 3.곱셈 4.나눗셈 5.종료");
+		System.out.println("--------------------------");
+		try {
+			command=command(command);
+		if(command==1) {
+			add(num1,num2);
+			}
+		if(command==2) {
+			subtract(num1,num2);
+		}
+		if(command==3) {
+			result=multiply(num1,num2);
+			System.out.println("계산결과 :" + result);
+		}
+		if(command==4) {
+			result=divide(num1,num2);
+			System.out.println("계산결과 :" + result);
+		}
+		if(command==5) {
+			System.out.println("종료되었습니다");
+			break;
+		}
 		
-		result=num1+num2; // 메인에서 가져온 num1과 num2를 바탕으로 연산후
-		
-		return result; // 결과를 메인으로 리턴함
+		}
+		 catch(InputMismatchException a) {
+				System.out.println("Error!!! InputMismatchException"+"\n"+"올바른 커맨드가 아닙니다. 커맨드는 숫자로 입력하셔야합니다." +" \n" + "프로그램을 다시 시작합니다." + "\n");
+				System.out.println("-----------");
+				System.out.println("재시작 중");
+				System.out.println("-----------"+ "\n");
+	}
+		}
 		
 	}
 	
-	public int sub(int num1, int num2) {
-		int result;
-		
-		result=num1-num2;
-		
-		return result;
-		
-	}
-	
-	public void sum1(int num1, int num2) { //반환값이 없는 메소드 void
-		int result;
-		
-		result=num1+num2;
-		
-		System.out.println("리턴 값 : " + result);
-		
-	}
 	public void add(int num1,int num2) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("계산할 숫자를 입력하세요.");
+		num1=scan.nextInt();
+		System.out.println("계산할 숫자를 입력하세요.");
+		num2=scan.nextInt();
 		int result=num1+num2;
 		System.out.println("덧셈 결과 :" +result);
 	}
-	public void subtract(int num1,int num2) {
+	private void subtract(int num1,int num2) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("계산할 숫자를 입력하세요.");
+		num1=scan.nextInt();
+		System.out.println("계산할 숫자를 입력하세요.");
+		num2=scan.nextInt();
 		int result=num1-num2;
 		System.out.println("뺄셈 결과 :"+result);
 	}
 	
-	public int multiply2(int num1,int num2) {
+	private int multiply(int num1,int num2) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("계산할 숫자를 입력하세요.");
+		num1=scan.nextInt();
+		System.out.println("계산할 숫자를 입력하세요.");
+		num2=scan.nextInt();
 		int result=num1*num2;
 		return result;
 	}
-	public int divide2(int num1,int num2) {
+	private int divide(int num1,int num2) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("계산할 숫자를 입력하세요.");
+		num1=scan.nextInt();
+		System.out.println("계산할 숫자를 입력하세요.");
+		num2=scan.nextInt();
 		int result=num1/num2;
 		return result;
+		
+	}
+	private int command(int command) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("커맨드를 입력하세요.");
+		command=scan.nextInt();
+		return command;
 	}
 	
 	
--- 1216-1/src/test1/CalculatorMain.java
+++ 1216-1/src/test1/CalculatorMain.java
@@ -1,64 +1,15 @@
 package test1;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class CalculatorMain {
 
 	public static void main(String[] args) {
 		//계산기 메소드 활용해보기.
-		Scanner scan = new Scanner(System.in);
-		int num1=9;
-		int num2=5;
-		int result=0;
-		int command=0;
 		Calculator ca = new Calculator();
-		ca.sum(num1, num2);
-		System.out.println("더하기 결과 : " +ca.sum(num1, num2));
-		ca.sub(num1, num2);
-		System.out.println("빼기 결과 : " + ca.sub(num1, num2));
-		ca.sum1(num1, num2);
-		while(true) {
-			System.out.println("--------------------------");
-			System.out.println("1.덧셈 2.뺄셈 3.곱셈 4.나눗셈 5.종료");
-			System.out.println("--------------------------");
-			System.out.println("커맨드를 입력하세요.");
-			try {
-			command=scan.nextInt();
-			if(command==1) {
-				System.out.println("계산할 숫자를 입력하세요.");
-				ca.add(scan.nextInt(),scan.nextInt());}
-			if(command==2) {
-				System.out.println("계산할 숫자를 입력하세요.");
-				ca.subtract(scan.nextInt(),scan.nextInt());
-			}
-			if(command==3) {
-				System.out.println("계산할 숫자를 입력하세요.");
-				result=ca.multiply2(scan.nextInt(),scan.nextInt());
-				System.out.println("계산결과 :" + result);
-			}
-			if(command==4) {
-				System.out.println("계산할 숫자를 입력하세요.");
-				result=ca.divide2(scan.nextInt(),scan.nextInt());
-				System.out.println("계산결과 :" + result);
-			}
-			if(command==5) {
-				System.out.println("종료되었습니다");
-				break;
-			}
-			}
-			 catch(InputMismatchException a) {
-					System.out.println("올바른 커맨드가 아닙니다. 커맨드는 숫자로 입력하셔야합니다." +" \n" + "프로그램을 다시 시작합니다." + "\n");
-					System.out.println("-----------");
-					System.out.println("재시작 중");
-					System.out.println("-----------"+ "\n");
-					main(args);
-		}
+			ca.calpro();
 			
-		}
 		
 		
-		scan.close();
+		
 	}
 
 }
