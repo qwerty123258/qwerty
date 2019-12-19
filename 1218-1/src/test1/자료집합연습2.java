443548e30337727cf987e2e9d81d587dac40d220
5 <5@5-PC>
2019-12-19 16:51:07 +0900

    12월 19일 커밋

--- 1218-1/src/test1/자료집합연습2.java
+++ 1218-1/src/test1/자료집합연습2.java
@@ -4,6 +4,9 @@ public class 자료집합연습2 {
 
 	public static void main(String[] args) {
 		String max=null;//최대값 출력용 변수
+		String min=null;//최소값 출력용
+		int max1=Integer.MIN_VALUE; //최대길이 비교용 변수
+		int min1=Integer.MAX_VALUE; //최소길이 비교용
 		List<String> wordList = new ArrayList<String>();
 		Scanner scan = new Scanner(System.in);
 		for(int i=0; i<=4; i++) {
@@ -12,11 +15,18 @@ public class 자료집합연습2 {
 			wordList.add(i,input);
 		}
 		for(int i=0; i<wordList.size(); i++) {
-				if(wordList.get(i).length()>wordList.get(i).length()) {
+				if(wordList.get(i).length()>max1) {
+					max1=wordList.get(i).length();
 					max=wordList.get(i);
 			}
+				if(wordList.get(i).length()<min1) {
+					min1=wordList.get(i).length();
+					min=wordList.get(i);
+			}
 		}
-		System.out.println(max);
+		System.out.println(wordList);
+		System.out.println("제일 짧은 단어:" +min);
+		System.out.println("제일 긴 단어:" +max);
 
 	}
 	
