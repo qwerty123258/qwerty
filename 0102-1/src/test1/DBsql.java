1c5b999adfc6c7c0c3d46bc5a7f79562044cfa6a
5 <5@5-PC>
2020-01-07 11:19:59 +0900

    변수 이름 정리

--- 0102-1/src/test1/DBsql.java
+++ 0102-1/src/test1/DBsql.java
@@ -5,7 +5,7 @@
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String NowUser = "";
+		String OtherUser = "";
 		Scanner scan = new Scanner(System.in);
 		boolean start=true;
 		
@@ -141,7 +141,7 @@
 			}
 			return locationB;
 		}
-		public void CityproPertyCheck(int locationA, String userA,String userB) {
+		public void CityproPertyCheck(int location,String userA,String userB) {
 			//설명:리턴된 값에 따라 해당 유저가 유저가 도시에 도착했을시 어느 도시인지 출력후     (김현) 
 			//도착한 곳이 공백도시인지,자신의 도시인지,타인의 도시인지,국세청인지,출발지,찬스인지에 따라 실행되야되는 기능이 다르므로 
 			//현재위치를 판단 후 도착한 곳에 따라 각기 다른 기능을 실행해주는 메소드를 생성.
@@ -151,25 +151,25 @@
 			String sql = "SELECT CITY,PRICE,PROPERTY FROM MONOPOLY WHERE CITYNO=?";  
 			try {  
 				if (userA.equals(userA)) { //게임을이용하는 유저는 2명이므로 어느유저가 이용하는지 판별하기위해 IF문을씀
-					NowUser = userB; //상대 유저를 B라고 지정
+					OtherUser = userB; //상대 유저를 B라고 지정
 				} 
 				else {
-					NowUser = userA; //반대의 경우는 A라고 지정.
+					OtherUser = userA; //반대의 경우는 A라고 지정.
 				}
 				pstmt = con.prepareStatement(sql);
-				pstmt.setInt(1, locationA);
+				pstmt.setInt(1, location);
 				rs = pstmt.executeQuery();
 				while (rs.next()) {
 					int price=rs.getInt("price");
-					if(locationA==1) { //1번위치는 출발지점이라 변해야할값이 없으므로 리턴  
+					if(location==1) { //1번위치는 출발지점이라 변해야할값이 없으므로 리턴  
 						return;   
 					}
-					else if (locationA == 5) { //국세청일땐 세금(지불)만 내게끔 IF문사용 
+					else if (location == 5) { //국세청일땐 세금(지불)만 내게끔 IF문사용 
 						System.out.println(rs.getString("city")+"에 방문했습니다.");
 						System.out.println(price + "를 지불합니다.");
 						UserMoneyTax(price, userA,userB); //유저가 5번에 위치했을때  메세지 출력후 세금메소드 실행
 					}
-					else if (locationA == 10) { //찬스 IF문
+					else if (location == 10) { //찬스 IF문
 						System.out.println(rs.getString("city")+"에 방문했습니다.");
 						Chance(userA);//유저가 10번에 위치했을때  메세지출력후 찬스 메소드실행
 					}
@@ -177,10 +177,10 @@
 						System.out.println("당신의 소유 도시인  "+rs.getString("city")+"에 방문했습니다.");
 						return;//자신의 소유지면 더 이상의 실행내용 없으므로 해당 메소드 종료
 					} 
-					else if ((rs.getString("property")).equals(NowUser)) {// 상대도시 IF문
+					else if ((rs.getString("property")).equals(OtherUser)) {// 상대도시 IF문
 						System.out.print(rs.getString("city")+"에 방문했습니다.  ");
 						System.out.println(rs.getString("property") + "의 소유지 입니다. " + price + "를 지불합니다.");
-						UserMoneyPayment(price, locationA, userA,userB);//상대의 소유지일땐 메세지 출력후 지불 메소드 진행
+						UserMoneyPayment(price, location, userA,userB);//상대의 소유지일땐 메세지 출력후 지불 메소드 진행
 					} 
 					else { //출발지,국세청,자신의 도시,타인의 도시 모두 아닌 공백지일때 IF문
 						System.out.println(rs.getString("city")+"에 방문했습니다.");
@@ -191,7 +191,7 @@
 						int input1 = scan.nextInt();
 						switch (input1) {// 메세지출력후 구매/패스 고르는 SWITCH문
 						case 1:
-							CityPurchase(price, userA,userB,locationA);//무소유지 도시 구매 메소드
+							CityPurchase(price, userA,location);//무소유지 도시 구매 메소드
 							String enter=scan.nextLine(); //도시 구매를 할때는 nextLINE() 메소드를 사용하는데 이는 Enter전까지 입력받으므로 남아있는 Enter를 소진시키는 용도
 							break;
 						case 2:
@@ -204,7 +204,7 @@
 				e.printStackTrace();
 			}
 		}
-		public void Chance(String userA) { //1P,2P가 찬스 지역에 위치하는 실행되는 메소드,랜덤한 확률에 의해 실행.(전석종)
+		public void Chance(String user) { //1P,2P가 찬스 지역에 위치하는 실행되는 메소드,랜덤한 확률에 의해 실행.(전석종)
 			int chance=(int)(Math.random()*3)+1;
 			System.out.println("찬스 발동");
 			if(chance==1) {
@@ -212,7 +212,7 @@
 				String sql="update userlist set money=money+money/10 where name=?";
 				try {
 				pstmt = con.prepareStatement(sql);
-				pstmt.setString(1, userA);
+				pstmt.setString(1, user);
 				pstmt.executeUpdate();
 				pstmt.close();
 				}
@@ -228,7 +228,7 @@
 					String sql="update userlist set money=money+1000 where name=?";
 					try {
 					pstmt = con.prepareStatement(sql);
-					pstmt.setString(1, userA);
+					pstmt.setString(1, user);
 					pstmt.executeUpdate();
 					pstmt.close();
 					}
@@ -240,7 +240,7 @@
 					String sql="update userlist set money=money-500 where name=?";
 					try {
 					pstmt = con.prepareStatement(sql);
-					pstmt.setString(1, userA);
+					pstmt.setString(1, user);
 					pstmt.executeUpdate();
 					pstmt.close();
 					}
@@ -254,7 +254,7 @@
 				String sql="update userlist set money=money-money/10 where name=?";
 				try {
 				pstmt = con.prepareStatement(sql);
-				pstmt.setString(1, userA);
+				pstmt.setString(1, user);
 				pstmt.executeUpdate();
 				pstmt.close();
 				}
@@ -308,11 +308,11 @@
 			}		
 			return false;
 		}
-		public void CityPurchase(int price, String userA,String userB,int locationA) { //도시를 구매하는 메소드
+		public void CityPurchase(int price, String user,int location) { //도시를 구매하는 메소드
 			String sql = "SELECT MONEY FROM USERLIST WHERE NAME=?";
 			try {
 				pstmt = con.prepareStatement(sql);
-				pstmt.setString(1, userA);
+				pstmt.setString(1, user);
 				rs = pstmt.executeQuery();
 				while (rs.next()) {
 					int saveMoney = rs.getInt("money"); //현재잔액을 얻어옴
@@ -322,21 +322,21 @@
 				if(saveMoney > price) {
 					sql = "select * from monopoly where cityno=?";
 					pstmt = con.prepareStatement(sql);
-					pstmt.setInt(1, locationA);
+					pstmt.setInt(1, location);
 					rs = pstmt.executeQuery();
 					while(rs.next()) {
-						System.out.println(userA+"가 "+ rs.getString("city")+"를 구매하였습니다.");
+						System.out.println(user+"가 "+ rs.getString("city")+"를 구매하였습니다.");
 					}
 					sql = "UPDATE MONOPOLY SET PROPERTY=? WHERE PRICE=?"; //도시를 구매하면 도시의 소유권 컬럼의 값이 변경.
 					pstmt = con.prepareStatement(sql);
-					pstmt.setString(1, userA);
+					pstmt.setString(1, user);
 					pstmt.setInt(2, price); //이 메소드를 호출한 곳에서 받아온 도시의 가격
 					pstmt.executeUpdate();
 					pstmt.close();
 					sql = "UPDATE USERLIST SET MONEY=MONEY-? WHERE NAME=?"; //도시를 구매하면 유저의 잔액이 변경
 					pstmt = con.prepareStatement(sql);
 					pstmt.setInt(1, price);
-					pstmt.setString(2, userA);
+					pstmt.setString(2, user);
 					pstmt.executeUpdate();
 					pstmt.close();
 				}
@@ -345,7 +345,7 @@
 				e.printStackTrace();
 			}
 		}
-		public void UserMoneyPayment(int price, int locationA, String userA, String userB) {
+		public void UserMoneyPayment(int price, int location, String userA, String userB) {
 			// 상대에서 도시의 가격만큼 값을 지불하는 메소드(김현)
 			//순서=값을 지불해야하는 유저의 잔액 조회>>1.지불할 금액보다 잔액이 적으면 파산후 게임종료
 			//1.돈이 있을 경우엔 지불 후 인수 유무 판단>>2.가격보다 넘게 돈 있으면 인수여부 판단
@@ -358,13 +358,13 @@
 					int saveMoney = rs.getInt("money");
 				if (saveMoney <= price) {// 현재 잔액보다 지불금액이 높거나 같다면 파산하고 게임이 종료되는 분기점
 					if (userA.equals(userA)) { //만약 위에서 A가 지불을 했다면 B는 돈을 받아야하므로 A와 B를 전환.
-						NowUser = userB;
+						OtherUser = userB;
 					} 
 					else {
-						NowUser = userA; //반대로 B가 지불을 했다면 B와 A를 전환.
+						OtherUser = userA; //반대로 B가 지불을 했다면 B와 A를 전환.
 					}
 					System.out.println(userA+"님이 파산하셨습니다");
-					System.out.println(NowUser+"가 승리하였습니다.");
+					System.out.println(OtherUser+"가 승리하였습니다.");
 					System.out.println("");
 					System.out.println("게임종료");
 					System.out.println("");
@@ -379,27 +379,27 @@
 					pstmt.executeUpdate();
 					pstmt.close();
 					if (userA.equals(userA)) { //만약 위에서 A가 지불을 했다면 B는 돈을 받아야하므로 A와 B를 전환.
-						NowUser = userB;
+						OtherUser = userB;
 					} 
 					else {
-						NowUser = userA; //반대로 B가 지불을 했다면 B와 A를 전환.
+						OtherUser = userA; //반대로 B가 지불을 했다면 B와 A를 전환.
 					}
 					sql = "UPDATE USERLIST SET MONEY=MONEY+? WHERE NAME=?";
 					pstmt = con.prepareStatement(sql);
 					pstmt.setInt(1, price);
-					pstmt.setString(2, NowUser);// 상대유저 넣는곳
+					pstmt.setString(2, OtherUser);// 상대유저 넣는곳
 					pstmt.executeUpdate();
 					pstmt.close();
-					TakeOverCity(price,userA,userB,locationA);
+					TakeOverCity(price,userA,userB,location);
 				}
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		public void TakeOverCity(int price,String userA,String userB,int locationA) {//현재 위치의 도시가 다른 사람의 소유지일경우 인수 유무를 결정하는 메소드(전석종)
+		public void TakeOverCity(int price,String userA,String userB,int location) {//현재 위치의 도시가 다른 사람의 소유지일경우 인수 유무를 결정하는 메소드(전석종)
 			int saveMoney=UserMoneySearch(userA);
-			String city=CitySearch(locationA);
+			String city=CitySearch(location);
 			if(saveMoney >= price) {
 				System.out.println("해당 도시를 인수하시겠습니까?");
 				System.out.println("");
@@ -414,21 +414,21 @@
 						pstmt.executeUpdate();
 						pstmt.close();
 						if (userA.equals(userA)) { //만약 위에서 A가 지불을 했다면 B는 돈을 받아야하므로 A와 B를 전환.
-							NowUser = userB;
+							OtherUser = userB;
 						} 
 						else {
-							NowUser = userA; //반대로 B가 지불을 했다면 B와 A를 전환.
+							OtherUser = userA; //반대로 B가 지불을 했다면 B와 A를 전환.
 						}
 						sql = "UPDATE USERLIST SET MONEY=MONEY+? WHERE NAME=?";
 						pstmt = con.prepareStatement(sql);
 						pstmt.setInt(1, price);
-						pstmt.setString(2, NowUser);// 상대유저 넣는곳
+						pstmt.setString(2, OtherUser);// 상대유저 넣는곳
 						pstmt.executeUpdate();
 						pstmt.close();
 						sql = "UPDATE monopoly SET property=? WHERE cityno=?"; //도시의 소유권 변경
 						pstmt = con.prepareStatement(sql);
 						pstmt.setString(1, userA);
-						pstmt.setInt(2, locationA);
+						pstmt.setInt(2, location);
 						pstmt.executeUpdate();
 						System.out.println(userA+"가 " +city+" 를(을) 인수하였습니다.");
 						String enter=scan.nextLine(); //도시 인수를 할때는 next() 메소드를 사용하는데 이는 Enter전까지 입력받으므로 남아있는 Enter를 소진시키는 용도
@@ -443,12 +443,12 @@
 				}
 			}
 		}
-		public void UserMoneyBonus(String userA) {// 1P,2P가 출발지점을 지나면 추가 월급을 제공해주는 메소드(전석종)
+		public void UserMoneyBonus(String user) {// 1P,2P가 출발지점을 지나면 추가 월급을 제공해주는 메소드(전석종)
 			String sql = "UPDATE USERLIST SET MONEY=MONEY+? WHERE NAME=?";
 			try {
 				pstmt = con.prepareStatement(sql);
 				pstmt.setInt(1, 200);
-				pstmt.setString(2, userA);
+				pstmt.setString(2, user);
 				pstmt.executeUpdate();
 				pstmt.close();
 				System.out.println("출발지점을 만나 보너스를 받습니다.");
@@ -467,12 +467,12 @@
 					int saveMoney = rs.getInt("money");
 				if (saveMoney <= price) {// 현재 잔액보다 지불금액이 높거나 같다면 파산하고 게임이 종료되는 분기점
 					if (userA.equals(userA)) {
-						NowUser = userB;
+						OtherUser = userB;
 					} else {
-						NowUser = userA;
+						OtherUser = userA;
 					}
 					System.out.println(userA+"님이 파산하셨습니다");
-					System.out.println(NowUser+"가 승리하였습니다.");
+					System.out.println(OtherUser+"가 승리하였습니다.");
 					System.out.println("");
 					System.out.println("");
 					System.out.println("게임종료");
@@ -510,11 +510,11 @@
 				Monopolymain.main(null);
 			}
 		}
-		public String CitySearch(int locationA) { //현재 위치에 맞는 도시번호를 조회하여 해당 도시의 이름을 반환해주는 메소드(전석종)
+		public String CitySearch(int location) { //현재 위치에 맞는 도시번호를 조회하여 해당 도시의 이름을 반환해주는 메소드(전석종)
 			String sql = "SELECT * FROM MONOPOLY WHERE cityno=?";
 			try {// 도시보유
 				pstmt = con.prepareStatement(sql);
-				pstmt.setInt(1, locationA);
+				pstmt.setInt(1, location);
 				rs = pstmt.executeQuery();
 				while (rs.next()) {
 					return rs.getString("city");
@@ -635,12 +635,12 @@
 			}
 			return name;
 	}
-		public void WinScoreInsertDB(String userA) { //승리 스코어를 DB에 반영하는 메소드(전석종)
+		public void WinScoreInsertDB(String user) { //승리 스코어를 DB에 반영하는 메소드(전석종)
 			String sql="update userlist set win=win+? where name=?";
 			try {
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, 1);
-			pstmt.setString(2, userA);
+			pstmt.setString(2, user);
 			pstmt.executeUpdate();
 			pstmt.close();
 			}
@@ -648,12 +648,12 @@
 			e.printStackTrace();
 			}
 		}
-		public void LoseScoreInsertDB(String userA) { //패배 스코어를 DB에 반영하는 메소드(전석종)
+		public void LoseScoreInsertDB(String user) { //패배 스코어를 DB에 반영하는 메소드(전석종)
 			String sql="update userlist set lose=lose+? where name=?";
 			try {
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, 1);
-			pstmt.setString(2, userA);
+			pstmt.setString(2, user);
 			pstmt.executeUpdate();
 			pstmt.close();
 			}
